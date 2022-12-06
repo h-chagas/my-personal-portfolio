2b702e5dfb4f2da8973c92cc4708eb4a3fde7298
Hugo Chagas <hugochagasuk@gmail.com>
2022-12-06 15:58:05 +0000

    create navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import '../styles/Navbar.css'
+import MenuIcon from '@mui/icons-material/Menu';
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <div className='toggleButton'>
+            <button>
+                <svg data-testid="MenuIcon"></svg>
+            </button>
+        </div>
+        <div className='links'>
+            <Link to="/"> Home </Link>
+            <Link to="/projects"> Projects </Link>
+            <Link to="/experience"> Experience </Link>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
