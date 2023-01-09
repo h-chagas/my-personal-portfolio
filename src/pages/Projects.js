841763e0a0c076b31f93aed76379c9057326bb31
Hugo Chagas <hugochagasuk@gmail.com>
2023-01-09 15:05:51 +0000

    fetch github API and map all of them

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,27 @@
+import { useState, useEffect } from 'react';
+import { RepositoryItem } from "../components/RepositoryItem";
+import "../styles/RepositoryItem.css"
+
+
 export const Projects = () => {
 
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(()=> {
+        fetch('https://api.github.com/users/h-chagas/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data));
+    }, []
+);
+
     return (
-        <p>Projects</p>
+        <section className="repository-list">
+        <h1 className='h1-repo'>Repository List</h1>
+        <ul className='ul-repo'>
+            {repositories.map(repository => {
+                return <RepositoryItem key={repository.name} repository={repository} />
+            })}
+        </ul>
+    </section>
     )
 }
